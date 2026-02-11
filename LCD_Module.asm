68f58839f49150b2b18b07b71370fb30205c8dc9
xuwanjing49-lgtm <xuwanjing49@gmail.com>
2026-02-10 17:04:54 -0800

    Update LCD_Module.asm

--- LCD_Module.asm
+++ LCD_Module.asm
@@ -34,7 +34,7 @@ LCD_pulse:
     ret
 
 LCD_byte:
-    ; 发送高 4 位
+    
     mov c, ACC.7
     mov LCD_D7, c
     mov c, ACC.6
@@ -44,7 +44,7 @@ LCD_byte:
     mov c, ACC.4
     mov LCD_D4, c
     lcall LCD_pulse
-    ; 发送低 4 位
+    
     mov c, ACC.3
     mov LCD_D7, c
     mov c, ACC.2
@@ -106,58 +106,51 @@ PrintString:
 PrintDone:
     ret
 
-; ====================================================================
-; ====================================================================
-; LCD_ShowSettings - 参数设置页面显示函数
-; 功能：按照 Line 1: SOAK, Line 2: REFL 格式显示设定值
-; ====================================================================
 LCD_ShowSettings:
-    ; --- 第一行：显示 SOAK 设定值 ---
-    mov a, #080H        ; 移动光标到第一行起始
+  
+    mov a, #080H        
     lcall LCD_cmd
     mov dptr, #STR_SOAK_LBL
-    lcall PrintString   ; 显示 "SOAK "
+    lcall PrintString   
 
-    ; 打印设定温度 (假设地址在 Soak_Temp)
-    mov r0, #Soak_Temp  ; 指向主文件定义的 Soak_Temp (0x37)
+    
+    mov r0, #Soak_Temp  
     lcall LCD_Print3BCD_R0
     mov a, #'C'
     lcall LCD_data
-    mov a, #' '         ; 空格
+    mov a, #' '         
     lcall LCD_data
 
-    ; 打印设定时间
-    mov r0, #Soak_Time  ; 指向 Soak_Time (0x3B)
+    mov r0, #Soak_Time  
     lcall LCD_Print2BCD_R0
     mov a, #'s'
     lcall LCD_data
 
-    ; --- 第二行：显示 REFL 设定值 ---
-    mov a, #0C0H        ; 移动光标到第二行起始
+    
+    mov a, #0C0H        
     lcall LCD_cmd
     mov dptr, #STR_REFL_LBL
-    lcall PrintString   ; 显示 "REFL "
+    lcall PrintString   
 
-    ; 打印设定温度
-    mov r0, #Reflow_Temp ; 指向 Reflow_Temp (0x3F)
+    
+    mov r0, #Reflow_Temp 
     lcall LCD_Print3BCD_R0
     mov a, #'C'
     lcall LCD_data
     mov a, #' '
     lcall LCD_data
 
-    ; 打印设定时间
-    mov r0, #Reflow_Time ; 指向 Reflow_Time (0x43)
+    
+    mov r0, #Reflow_Time 
     lcall LCD_Print2BCD_R0
     mov a, #'s'
     lcall LCD_data
     ret
 
-; --- 辅助函数：打印 3 位数字 (含百位) ---
+
 LCD_Print3BCD_R0:
     push acc
-    ; 打印百位 (读取当前地址的高位字节，假设 ds 分配了多个位)
-    ; 如果你的设置值只有两位，可以略过此步
+
     inc r0              
     mov a, @r0
     anl a, #0Fh
@@ -168,22 +161,21 @@ LCD_Print3BCD_R0:
     pop acc
     ret
 
-; --- 辅助函数：打印 2 位数字 (个、十位) ---
 LCD_Print2BCD_R0:
     push acc
     mov a, @r0
     swap a
     anl a, #0Fh
     add a, #'0'
-    lcall LCD_data      ; 打印十位
+    lcall LCD_data      
     mov a, @r0
     anl a, #0Fh
     add a, #'0'
-    lcall LCD_data      ; 打印个位
+    lcall LCD_data      
     pop acc
     ret
 LCD_ShowRun:
-    mov a, #080H        ; 第一行显示温度
+    mov a, #080H        
     lcall LCD_cmd
     mov dptr, #STR_T
     lcall PrintString
@@ -191,13 +183,13 @@ LCD_ShowRun:
     mov dptr, #STR_TJ
     lcall PrintString
 
-    mov a, #0C0H        ; 第二行显示时间和状态
+    mov a, #0C0H        
     lcall LCD_cmd
     lcall LCD_PrintTime2_Elapsed
     mov dptr, #STR_SEC
     lcall PrintString
 
-    ; 状态文字分支
+    
     jb e_shutdown_flag, _MSG_ERR
     mov a, State_flag
     cjne a, #0, _MSG_S1
@@ -227,16 +219,16 @@ _MSG_ERR:
     lcall PrintString
     ret
 
-; --- 内部辅助子程序 ---
 
-; 打印 R0 指向的 BCD 温度 (3位)
+
+
 LCD_PrintTemp3_R0:
-    inc r0              ; 移动到百位地址
+    inc r0              
     mov a, @r0
     anl a, #0Fh
     add a, #'0'
     lcall LCD_data
-    dec r0              ; 移动回十位个位地址
+    dec r0            
     mov a, @r0
     swap a
     anl a, #0Fh
@@ -248,32 +240,28 @@ LCD_PrintTemp3_R0:
     lcall LCD_data
     ret
 
-; 打印当前温度 (读取 33H 和 34H)
-; --- 内部辅助子程序 ---
-
-; 打印当前温度 (读取 Temp_High=0x35 和 Temp_Low=0x34)
 LCD_PrintTemp3_curr:
     push acc
     push ar0
     
-    ; 1. 打印百位
-    mov r0, #Temp_High       ; 显式使用 EQU 名字 (0x35)
+    
+    mov r0, #Temp_High       
     mov a, @r0
     anl a, #0Fh
     add a, #'0'
     lcall LCD_data
     
-    ; 2. 打印十位
-    mov r0, #Temp_Low        ; 显式使用 EQU 名字 (0x34)
+    
+    mov r0, #Temp_Low        
     mov a, @r0
-    swap a                   ; 取高4位（十位）
+    swap a                   
     anl a, #0Fh
     add a, #'0'
     lcall LCD_data
     
-    ; 3. 打印个位
-    mov a, @r0               ; 重新读一次低位字节
-    anl a, #0Fh              ; 取低4位（个位）
+    
+    mov a, @r0               
+    anl a, #0Fh              
     add a, #'0'
     lcall LCD_data
     
@@ -281,19 +269,19 @@ LCD_PrintTemp3_curr:
     pop acc
     ret
 
-; 打印运行时间 (读取 Time_Elapsed_High=0x33 和 Time_Elapsed_Low=0x32)
+
 LCD_PrintTime2_Elapsed:
     push acc
     push ar0
 
-    ; 1. 打印百位
+    
     mov r0, #Time_Elapsed_High ; (0x33)
     mov a, @r0
     anl a, #0Fh
     add a, #'0'
     lcall LCD_data
     
-    ; 2. 打印十位
+    
     mov r0, #Time_Elapsed_Low  ; (0x32)
     mov a, @r0
     swap a
@@ -301,7 +289,7 @@ LCD_PrintTime2_Elapsed:
     add a, #'0'
     lcall LCD_data
     
-    ; 3. 打印个位
+    
     mov a, @r0
     anl a, #0Fh
     add a, #'0'
@@ -326,3 +314,4 @@ ST_SOAK: DB 'SOAKING   ',0
 ST_ERROR:DB 'ERROR     ',0
 STR_SOAK_LBL: DB 'SOAK ', 0
 STR_REFL_LBL: DB 'REFL ', 0
+
